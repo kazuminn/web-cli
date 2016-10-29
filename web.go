6e9b04d1ad9012eecc83a4e9cce883b56f48cc5b
kazuminn <e145702@ie.u-ryukyu.ac.jp>
2016-10-29 14:32:24 +0900

    gofmt

--- web.go
+++ web.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net/http"
-	"os"
+		"flag"
+		"fmt"
+		"net/http"
+		"os"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
+		"github.com/labstack/echo"
+		"github.com/labstack/echo/engine/standard"
 )
 
 func main() {
-	// help
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		flag.PrintDefaults()
-	}
+		// help
+		flag.Usage = func() {
+			fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+			flag.PrintDefaults()
+		}
 
-	//flag 処理
-	defaultPort := ":1323"
+		//flag 処理
+		defaultPort := ":1323"
 
-	var (
-		defaultport = flag.String("defaultport", defaultPort, ":port")
-	)
-	port := *defaultport
-	flag.Parse()
+		var (
+			defaultport = flag.String("defaultport", defaultPort, ":port")
+		)
+		port := *defaultport
+		flag.Parse()
 
-	//web server
-	e := echo.New()
-	e.GET("/", get)
-	e.Run(standard.New(port))
+		//web server
+		e := echo.New()
+		e.GET("/", get)
+		e.Run(standard.New(port))
 }
 
 func get(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, World!")
 }
