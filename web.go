c9c80d171e90698a2058596a1ea7119f72950981
kazuminn <e145702@ie.u-ryukyu.ac.jp>
2016-10-29 13:54:03 +0900

    関数を分けた

--- web.go
+++ web.go
@@ -2,14 +2,17 @@ package main
 
 import (
     "net/http"
+
     "github.com/labstack/echo"
     "github.com/labstack/echo/engine/standard"
 )
 
 func main() {
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
+    e.GET("/", get)
     e.Run(standard.New(":1323"))
+}
+
+func get(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, World!")
 }
